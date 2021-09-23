ebe54f9c026e95929c9daa837c000e50565342e1
Satvik <satvikkalyan@gmail.com>
2021-09-23 16:44:23 +0530

    Production grade Code Docker File

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -13,3 +13,19 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+ 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
